94262c218bf0767af91129288908b32e3e75649a
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-07-12 22:14:53 +0545

    completed

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
-const socketio = require("socket.io");
+const socket = require("socket.io");
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socket(server);
 app.set("view engine", "ejs");
 app.set(express.static(path.join(__dirname, "public")));
 io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("recieve-location", { id: socketio.id, ...data });
+  });
   console.log("Connection Successfully");
 });
 
 const port = 3000;
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+  res.render("index");
+});
 server.listen(port, () =>
   console.log(`Example app listening on port ${port}!`)
 );
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,6 @@
+const { localsName } = require("ejs");
+const { Socket } = require("socket.io");
+
 const socket = io();
 if (navigator.geolocation) {
   navigator.geolocation.watchPosition(
@@ -15,3 +18,18 @@ if (navigator.geolocation) {
     }
   );
 }
+L.map("map").setView([0, 0], 10);
+L.titlelayer("https://{s}.tile.opemstreetmap.org/{z}{x}{y}.png", {
+  attribution: "Openstreetmap",
+}).addTo(map);
+
+const markers = {};
+Socket.on("recieve-location", (data) => {
+  const { id, Latitude, Longitude } = data;
+  map.setView([Latitude, Longitude], 16);
+  if (markers[id]) {
+    markers[id].setLatLng([Latitude, Longitude]);
+  } else {
+    markers[os] = L.markers([Latitude, Longitude]);
+  }
+});
